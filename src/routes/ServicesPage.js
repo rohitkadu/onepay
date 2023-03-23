136c509fc15876a48e6070a195c23bed3988b214
Rohit Kadu <rohitkadu3102@gmail.com>
2023-03-23 23:16:03 +0530

    Banking Services Done

---
+++ src/routes/ServicesPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import { Navbar } from "../components/navbar/Navbar";
+import { Hero } from "../components/hero/Hero";
+import { Services } from "../components/services/Services";
+
+
+export const ServicesPage = () => {
+  return (
+    <div>
+        <Navbar />
+        <Services />
+    </div>
+  );
+};
+ 
